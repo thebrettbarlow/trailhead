88f88489bb8281659181003852f691835e1cb301
Brett Barlow <me@brettbarlow.com>
2025-01-12 05:37:45 -0800

    feat: dev beginner metadata

---
+++ force-app/dev/beginner/lwc/housingMap/housingMap.js
@@ -0,0 +1,34 @@
+/**
+ * housingMap.js
+ */
+
+import { LightningElement, wire } from 'lwc';
+import getHouses from '@salesforce/apex/HouseService.getRecords';
+
+export default class HousingMap extends LightningElement {
+  mapMarkers;
+  error;
+
+  // TODO: resume at "Add the Component to the App Home"
+
+  @wire(getHouses)
+  wiredHouses({ error, data }) {
+    if (data) {
+      // Use JavaScript Map function to transform the Apex method response wired to the component into the format required by lightning-map
+      this.mapMarkers = data.map((element) => {
+        return {
+          location: {
+            Street: element.Address__c,
+            City: element.City__c,
+            State: element.State__c
+          },
+          title: element.Name
+        };
+      });
+      this.error = undefined;
+    } else if (error) {
+      this.error = error;
+      this.mapMarkers = undefined;
+    }
+  }
+}
