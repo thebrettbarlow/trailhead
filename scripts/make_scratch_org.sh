9bb351b34197614a85ab2767a0fd00104fcce10d
Brett Barlow <me@brettbarlow.com>
2025-01-17 05:49:15 -0800

    docs: updated notes about scratch org

--- scripts/make_scratch_org.sh
+++ scripts/make_scratch_org.sh
@@ -2,6 +2,26 @@
 
 set -e
 
+##
+# Creates a scratch org and deploys metadata to it
+#
+# This is useful when you'd like to restart where you left off based on metadata
+# saved in the repository. It will not create a Trailhead Playground. Instead,
+# it will create a scratch org and you will need to connect the org to Trailhead
+# to complete the challenges.
+#
+# Follow these steps to connect the org to Trailhead:
+#
+# 1. Run this script to create the scratch org
+# 2. Deploy metadata to the scratch org
+# 3. Optionally deploy data to the scratch org (not required for all challenges)
+# 4. Use `npm run sf:password:get` to get the password for the scratch org
+# 4. Open the Trailhead UI
+# 5. Click on the org picker in the "Hands-on Challenge" section
+# 6. Click on "Connect Org"
+# 7. Login with the username and password from step 4
+##
+
 target_org="trailhead--scratch"
 
 echo "Creating scratch org..."
@@ -10,20 +30,6 @@ sf org create scratch \
   --definition-file=config/project-scratch-def.json \
   --duration-days=30 \
   --set-default
-
-echo "Deploying metadata..."
-sf project deploy start \
-  --target-org="${target_org}" \
-  --source-dir=force-app \
-  --wait=10 \
-  --ignore-conflicts \
-  --verbose
-
-echo "Running all tests..."
-sf apex test run \
-  --target-org="${target_org}" \
-  --wait=10 \
-  --code-coverage
 
 echo "Opening scratch org..."
 sf org open \
